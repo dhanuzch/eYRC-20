32ba6c80e409ef59c77f882ac9ff03415b49e315
dhanuzch <mr.dhanush29@gmail.com>
2021-02-03 13:27:07 +0530

    Added Task5

--- pkg_task2/scripts/quart-euler.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-import rospy
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
-
-
-rpy = quaternion_from_euler(0.043691,0.023889,-0.000144)
-
-print rpy
